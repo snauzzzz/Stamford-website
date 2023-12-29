7eefd1f3deb46a9e8dca7254033d1dc6a2cd86b3
NguyenHaThaiSon <mathsislife314@gmail.com>
2023-12-29 15:23:12 +0800

    Move JS script for the navigation bar into its own file

---
+++ scripts/nav-bar.js
@@ -0,0 +1,25 @@
+//Toggle logout visibility
+const welcomeButton = document.querySelector('.js-welcome-button');
+
+welcomeButton.addEventListener('click', () => {
+    const logout = document.querySelector('.js-logout');
+
+    if (logout.classList.contains('logout-visible')) {
+        logout.classList.remove('logout-visible')
+    } else {
+        logout.classList.add('logout-visible');
+    }
+});
+
+//Toggle course info modal
+const allocationModal = document.querySelector('.js-allocation-modal');
+const courseInfoButton = document.querySelector('.js-view-allocation-button');
+const closeModalButton = document.querySelector('.js-close-modal-button')
+
+courseInfoButton.addEventListener('click', () => {
+    allocationModal.classList.add('allocation-modal-active');
+});
+
+closeModalButton.addEventListener('click', () => {
+    allocationModal.classList.remove('allocation-modal-active');
+})
