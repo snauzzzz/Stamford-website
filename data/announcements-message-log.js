57120605b6604975cafe48f950e471fdd680b017
NguyenHaThaiSon <mathsislife314@gmail.com>
2024-01-01 21:03:51 +0800

    Build annoucements items modals and HTML rendering

--- data/announcements-message-log.js
+++ data/announcements-message-log.js
@@ -1,38 +1,101 @@
 export const message = [{ 
-    tagId: 'CCA',
+    tagId: 'Admin',
     content: 'Revision of 2024 School Fees for Singapore Permanent Resident (SPR) Students',
     pinned: true,
-    date: '08 Nov 2023'
-}, { 
-    tagId: 'Student Devt',
-    content: 'Revision of 2024 School Fees for Singapore Permanent Resident (SPR) Students',
-    pinned: false,
-    date: '08 Nov 2023'
+    date: '08 Nov 2023',
+    modal: {
+        title: 'Revision of 2024 School Fees for Singapore Permanent Resident (SPR) Students',
+        additionalDetails: `Dear Parents/Guardians of Y1-6 SPR Students,
+
+        Please refer to the attached letter with regard to the revision of school fees for Year 2024.`,
+        attachment: '2024 School Fees for SPR.pdf',
+        author: 'MS NG HWEE KEANG',
+        addedDate: '2023-11-08 1:19 PM'
+    }
 }, { 
     tagId: 'Admin',
-    content: 'Gojo revival',
-    pinned: false,
-    date: '08 Nov 2023'
-}, { 
-    tagId: 'CCA',
     content: 'Revision of 2024 School Fees for Singapore Permanent Resident (SPR) Students',
     pinned: true,
-    date: '08 Nov 2023'
+    date: '25 Oct 2023',
+    modal: {
+        title: '',
+        additionalDetails: '',
+        attachment: '',
+        author: '',
+        addedDate: ''
+    }
 }, { 
-    tagId: 'Student Devt',
-    content: 'Revision of 2024 School Fees for Singapore Permanent Resident (SPR) Students',
-    pinned: false,
-    date: '08 Nov 2023'
+    tagId: 'Admin',
+    content: 'Travel Declaration for Year-End Holiday',
+    pinned: true,
+    date: '23 Oct 2023',
+    modal: {
+        title: '',
+        additionalDetails: '',
+        attachment: '',
+        author: '',
+        addedDate: ''
+    }
+}, { 
+    tagId: 'Admin',
+    content: 'Nominations open for Caring Teacher Awards 2024! ',
+    pinned: true,
+    date: '03 Oct 2023',
+    modal: {
+        title: '',
+        additionalDetails: '',
+        attachment: '',
+        author: '',
+        addedDate: ''
+    }
 }, { 
     tagId: 'Academic',
-    content: 'Revision of 2024 School Fees for Singapore Permanent Resident (SPR) Students',
-    pinned: true,
-    date: '08 Nov 2023'
+    content: 'Third language and MSP withdrawal [Year 2-Year 4] 2024',
+    pinned: false,
+    date: '31 Dec 2023',
+    modal: {
+        title: '',
+        additionalDetails: '',
+        attachment: '',
+        author: '',
+        addedDate: ''
+    }
 }, { 
     tagId: 'Student Devt',
-    content: 'Revision of 2024 School Fees for Singapore Permanent Resident (SPR) Students',
-    pinned: true,
-    date: '08 Nov 2023'
+    content: 'Reminder on Road Safety In and Out of School',
+    pinned: false,
+    date: '30 Dec 2023',
+    modal: {
+        title: '',
+        additionalDetails: '',
+        attachment: '',
+        author: '',
+        addedDate: ''
+    }
+}, { 
+    tagId: 'Admin',
+    content: 'For International Students - Removal of GST Subsidy on National Examination Fees From 2024',
+    pinned: false,
+    date: '11 Dec 2023',
+    modal: {
+        title: '',
+        additionalDetails: '',
+        attachment: '',
+        author: '',
+        addedDate: ''
+    }
+}, { 
+    tagId: 'Admin',
+    content: 'Collection of October to December 2023 School Fees and GIRO Dates for Year 2024',
+    pinned: false,
+    date: '15 Sep 2023',
+    modal: {
+        title: '',
+        additionalDetails: '',
+        attachment: '',
+        author: '',
+        addedDate: ''
+    }
 }]
 
 export function sortMessage(message) {
--- scripts/announcement-item.js
+++ scripts/announcement-item.js
@@ -4,6 +4,8 @@ import { message, sortMessage } from "../data/announcements-message-log.js";
 export function renderAnnouncementList() {
     //Sort message to prioritize pinned 
     const sortedMessage = sortMessage(message);
+    //Assign id
+    let id = 0;
     
     let announcementsHTML = '';
     let background = false;
@@ -23,23 +25,78 @@ export function renderAnnouncementList() {
         let backgroundColor = background ? 'gray-background' : 'white-background'; 
         
         announcementsHTML += `
-            <div class="announcements-item ${backgroundColor}">
-                <button style="background-color: ${matchingTag.backgroundColor};">${matchingTag.id}</button>
+            <div class="announcements-item ${backgroundColor} js-announcements-item" data-id=${id}>
+                <button class="tag-button" style="background-color: ${matchingTag.backgroundColor};">${matchingTag.id}</button>
             
                 <div class="tooltip">
                     <p>${matchingTag.name}</p>
                 </div>
                 
-                <div class="announcements-item-content">
+                <div class="announcements-item-content js-announcements-item-content">
                     <p>${message.content}</p>
                     ${isPinned}
                 </div>
                 <div class="announcements-item-date"> - <span>${message.date}</span></div>
+                
+                <div class="announcements-modal js-announcements-modal-${id}" data-id=${id}>
+                    <div class="overlay"></div>
+                    <div class="announcements-modal-content">
+                        
+                        <div class="modal-header">
+                           Announcement
+                           <button class="announcements-close-modal-button js-close-modal-button" style="color: rgb(214,214,214);">&#x2715;</button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Title : <span>${message.modal.title}</span></p>
+                            <p style="margin-bottom: 5px">Additional Details :</p>
+                            <span style="display: block;">
+                                ${message.modal.additionalDetails}
+                            </span>
+                            <p>Attachment : <span>${message.modal.attachment}</p>
+                            <p>Posted by : <span>${message.modal.author}</p>
+                            <p>Added On : <span>${message.modal.addedDate}</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button class="js-close-modal-button">Close</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         `;
 
+        id++;
     });
 
     const announcementsLog = document.querySelector('.announcements-log');
     announcementsLog.innerHTML = announcementsHTML;
+
+    document.querySelectorAll('.js-announcements-item-content p')
+        .forEach((item) => {
+            item.addEventListener('click', () => {
+                // Get parent .js-announcements-item element
+                const parent = item.closest('.js-announcements-item');
+
+                // Get data-id from parent
+                const id = parent.dataset.id;
+
+                const modal = document.querySelector(`.js-announcements-modal-${id}`);
+
+                modal.classList.add('announcements-modal-active')
+            })
+        });
+
+    document.querySelectorAll('.js-close-modal-button')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            // Get parent .js-announcements-item element
+            const parent = button.closest('.js-announcements-item');
+
+            // Get data-id from parent
+            const id = parent.dataset.id;
+
+            const modal = document.querySelector(`.js-announcements-modal-${id}`);
+
+            modal.classList.remove('announcements-modal-active')
+        })
+    });
 }
